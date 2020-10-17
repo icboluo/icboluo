0e61801c4f233df6ccc21f493b887f52ddca1bdd
icboluo <1qaz@WSX>
2020-10-17 19:12:52 +0800

    singleton

--- icboluo-test/src/main/java/com/icboluo/designpattern/danli/Singleton01.java
+++
@@ -1,9 +0,0 @@
-package com.icboluo.designpattern.danli;
-
-public class Singleton01 {
-    private static Singleton01 instance = new Singleton01();
-
-    public static Singleton01 getInstance() {
-        return instance;
-    }
-}
---
+++ icboluo-test/src/main/java/com/icboluo/shejimoshi/singleton/SingletonTest08.java
@@ -0,0 +1,18 @@
+package com.icboluo.shejimoshi.singleton;
+
+/**
+ * 枚举 也可以防止反序列化重写创建新的对象 推荐使用
+ *
+ * @author icboluo
+ * @date 2020/10/17 18:20
+ */
+public enum SingletonTest08 {
+    /**
+     * 实例
+     */
+    instance;
+
+    public void sayOk() {
+        System.out.println("ok");
+    }
+}
