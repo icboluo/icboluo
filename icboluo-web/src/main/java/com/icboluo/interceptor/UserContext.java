f0119ddebb2e0bc5133471c3fec94e4848c0d971
icboluo <1139803720@lp>
2021-10-24 23:54:04 +0800

    update pom

--- icboluo-web/src/main/java/com/icboluo/interceptor/UserContext.java
+++ icboluo-web/src/main/java/com/icboluo/interceptor/UserContext.java
@@ -1,17 +1,45 @@
 package com.icboluo.interceptor;
 
+import com.icboluo.controller.ResultController;
 import com.icboluo.util.IcBoLuoException;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.NoArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
 import org.springframework.util.ObjectUtils;
+
+import javax.annotation.PostConstruct;
+import javax.annotation.Resource;
 
 /**
  * @author icboluo
  */
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
+@Component
+@Slf4j
 public class UserContext {
     private static final ThreadLocal<AdminUser> USER_CONTEXT = new ThreadLocal<>();
+
+    private static String role;
+
+    @PostConstruct
+    private void setRole() {
+        role = "post construct set value for static field, second execution";
+        log.warn(role);
+    }
+
+    /**
+     * 成员方法给静态变量赋值
+     *
+     * @param resultController 这个参数只是模拟DI而已，找一个spring容器中的赋值给静态变量即可
+     */
+    @Resource
+    private void setRole(ResultController resultController) {
+        role = "member di set value for static field, first execution";
+        log.warn(role);
+    }
+
 
     @AllArgsConstructor
     private static class AdminUser {
