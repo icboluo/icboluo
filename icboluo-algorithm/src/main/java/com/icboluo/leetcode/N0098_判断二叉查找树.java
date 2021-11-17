5bf56add45c90b970397e5fbc081bc8a72d81b60
icboluo <1139803720@lp>
2021-11-18 00:46:48 +0800

    tree

---
+++ icboluo-algorithm/src/main/java/com/icboluo/leetcode/N0098_判断二叉查找树.java
@@ -0,0 +1,52 @@
+package com.icboluo.leetcode;
+
+import com.icboluo.common.TreeNode;
+
+import java.util.Stack;
+
+/**
+ * @author icboluo
+ * @date 2021-11-17 23:40
+ */
+public class N0098_判断二叉查找树 {
+
+    public boolean isValidBST(TreeNode root) {
+        return isValidBST2(root);
+    }
+
+    public boolean isValidBST1(TreeNode root) {
+        return isValidBST1(root, Long.MAX_VALUE, Long.MIN_VALUE);
+    }
+
+    private boolean isValidBST1(TreeNode root, long maxValue, long minValue) {
+        if (root == null) {
+            return true;
+        }
+        if (root.val >= maxValue || root.val <= minValue) {
+            return false;
+        }
+        return isValidBST1(root.left, root.val, minValue)
+                && isValidBST1(root.right, maxValue, root.val);
+    }
+
+    public boolean isValidBST2(TreeNode root) {
+        Stack<TreeNode> stack = new Stack<>();
+        stack.push(root);
+        while (!stack.isEmpty()) {
+            TreeNode cur = stack.pop();
+            if (cur.left != null) {
+                if (cur.val < cur.left.val) {
+                    return false;
+                }
+                stack.push(cur.left);
+            }
+            if (cur.right != null) {
+                if (cur.val > cur.right.val) {
+                    return false;
+                }
+                stack.push(cur.right);
+            }
+        }
+        return true;
+    }
+}
---
+++ icboluo-algorithm/src/main/java/com/icboluo/leetcode/N0104_二叉树最大深度.java
@@ -0,0 +1,48 @@
+package com.icboluo.leetcode;
+
+import com.icboluo.common.TreeNode;
+
+import java.util.Stack;
+
+/**
+ * @author icboluo
+ * @date 2021-11-17 23:55
+ */
+public class N0104_二叉树最大深度 {
+
+    public int maxDepth(TreeNode root) {
+        return maxDepth2(root);
+    }
+
+    public int maxDepth1(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+        int left = maxDepth1(root.left);
+        int right = maxDepth1(root.right);
+        return left > right ? left + 1 : right + 1;
+    }
+
+    public int maxDepth2(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+        Stack<TreeNode> stack = new Stack<>();
+        int max = 0;
+        stack.push(root);
+        while (!stack.isEmpty()) {
+            int size = stack.size();
+            for (int i = 0; i < size; i++) {
+                TreeNode pop = stack.pop();
+                if (pop.left != null) {
+                    stack.push(pop.left);
+                }
+                if (pop.right != null) {
+                    stack.push(pop.right);
+                }
+            }
+            max++;
+        }
+        return max;
+    }
+}
