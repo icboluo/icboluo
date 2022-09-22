494ceb23a94a64e01720919283bc25fb81950922
icboluo <1139803720@lp>
2022-09-23 01:44:16 +0800

    add redis/sql

---
+++ icboluo-gateway/src/main/java/com/icboluo/EmbededRedisConfiguration.java
@@ -0,0 +1,28 @@
+package com.icboluo;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import redis.embedded.RedisServer;
+
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+
+@Configuration
+public class EmbededRedisConfiguration {
+
+    private final RedisServer redisServer;
+
+    public EmbededRedisConfiguration(@Value("${spring.redis.port}") final int redisPort) {
+        this.redisServer = RedisServer.builder().port(redisPort).setting("maxmemory 256m").build();
+    }
+
+    @PostConstruct
+    public void startRedis() {
+        this.redisServer.start();
+    }
+
+    @PreDestroy
+    public void stopRedis() {
+        this.redisServer.stop();
+    }
+}
