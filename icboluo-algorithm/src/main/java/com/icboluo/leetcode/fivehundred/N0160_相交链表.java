dfefbe4b51f90c3449d2ebcb014e31d3dc188c46
1139803720 <1139803720@qq.com>
2022-10-05 21:35:03 +0800

    add 172/75/118/125

---
+++ icboluo-algorithm/src/main/java/com/icboluo/leetcode/fivehundred/N0160_相交链表.java
@@ -0,0 +1,20 @@
+package com.icboluo.leetcode.fivehundred;
+
+import com.icboluo.common.ListNode;
+
+class N0160_相交链表 {
+    public static void main(String[] args) {
+        ListNode listNode4 = new ListNode(4);
+    }
+
+    /**
+     * 双指针，a链表后面跟b，b链表后面跟a，两个指针相遇的地方就是链表相交的地方
+     *
+     * @param listNode01
+     * @param listNode02
+     */
+    private void m(ListNode listNode01, ListNode listNode02) {
+        ListNode aNode = listNode01;
+        ListNode bNode = listNode02;
+    }
+}
